0f40890103184e815b2f2eccbd64b6f01e9dabd7
Lucas Oliveira <lcsd.lucas@gmail.com>
2019-10-06 12:55:59 -0300

    Alterando para DB doo Heroku

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-    username: 'root',
-    password: 'root',
-    database: 'omnistack9',
-    host: '0.0.0.0',
+    username: 'bff3137e6d72c8',
+    password: '56283ed9',
+    database: 'heroku_b630b077e7db526',
+    host: 'us-cdbr-iron-east-05.cleardb.net',
     dialect: 'mysql'
 }
